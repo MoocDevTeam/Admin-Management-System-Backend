a12630712438d050f83a57c268d2f7981287245f
lilyjiangDev <jianglili2015@gmail.com>
2024-12-08 21:13:43 +1000

    MoocApi 1.0.0.5: Lily- Add integration test in Mooc.UnitTest and relevant method in UserService

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Mooc Project Team")]
 [assembly: AssemblyProduct("Mooc")]
 [assembly: AssemblyCopyright("Copyright © Your Company 2024")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
---
+++ Mooc.UnitTest/MoocCacheTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.Extensions.DependencyInjection;
+using Mooc.Core.Caching;
+
+namespace Mooc.UnitTest;
+
+public class MoocCacheTest : BaseTest
+{
+    [Test]
+    public async Task Test()
+    {
+        using (var scope = Factory.Services.CreateScope())
+        {
+            var moocCache = scope.ServiceProvider.GetRequiredService<IMoocCache>();
+            string key = "key1";
+            string val = "123dadfdsaf";
+            await moocCache.SetAsync(key, val, 5);
+            var cVal =await moocCache.GetAsync<string>(key);
+            Assert.That(val == cVal, "");
+        }
+    }
+}

a9ee0c63d05af66bf11bd1ff121a148248670c57
Yafei Deng <weihaianni@gmail.com>
2024-12-10 21:07:37 +1100

    Feat: added folders for controller, model, interface, and service

---
+++ MoocWebApi/Controllers/Course/MoocTeacherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoocWebApi.Controllers.Course
+{
+    public class MoocTeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

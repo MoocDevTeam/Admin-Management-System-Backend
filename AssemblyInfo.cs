42df2cbfbf5b020fb59e9cc5040a596582167520
Laura <laurajiao7@gmail.com>
2024-12-12 19:34:54 +1100

    feat:added enrollment files

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Mooc Project Team")]
 [assembly: AssemblyProduct("Mooc")]
 [assembly: AssemblyCopyright("Copyright © Your Company 2024")]
-[assembly: AssemblyVersion("1.0.0.8")]
-[assembly: AssemblyFileVersion("1.0.0.8")]
+[assembly: AssemblyVersion("1.0.0.10")]
+[assembly: AssemblyFileVersion("1.0.0.10")]

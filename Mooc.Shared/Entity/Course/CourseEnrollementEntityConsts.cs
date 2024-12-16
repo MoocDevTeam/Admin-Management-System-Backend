5aafa4a47b46c5b2bb5b8cd7434bf8681467e7ae
Laura <laurajiao7@gmail.com>
2024-12-16 19:07:33 +1100

    Added Enrollment Service

---
+++ Mooc.Shared/Entity/Course/CourseEnrollementEntityConsts.cs
@@ -0,0 +1,9 @@
+﻿namespace Mooc.Shared.Entity.Course;
+
+  public class EnrollementEntityConsts
+    {
+        public const int MaxStudents = 300;
+        public const int MinStudents = 1;   
+
+    }
+

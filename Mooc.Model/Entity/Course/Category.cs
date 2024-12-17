a5f03d654d147a515f65a7b5c88dc44d7fe60ac5
Yafei Deng <weihaianni@gmail.com>
2024-12-17 20:27:30 +1100

    MoocApi 1.0.0.16: Yafei Added catergory model, extention method for category table configueration and tested data migration.

--- Mooc.Model/Entity/Course/Category.cs
+++ Mooc.Model/Entity/Course/Category.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mooc.Model.Entity.Course
+﻿namespace Mooc.Model.Entity.Course
 {
-    public class Category
+    public class Category : BaseEntity
     {
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public string IconUrl { get; set; }
+        public long? ParentId { get; set; }
+
+        // foreign keys
+        public long CreatedByUserId { get; set; }
+        public long? UpdatedByUserId { get; set; }
+
+        // timestamp
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        // Nav user
+        public virtual User CreatedByUser { get; set; }
+        public virtual User UpdatedByUser { get; set; }
+
+        //Nav Category
+        public virtual Category ParentCategory { get; set; }
     }
 }

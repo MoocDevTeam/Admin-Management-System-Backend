04a6a26c4a9b27f59927f75ac982dbe2e4f8a8b7
Zhang-Haoran <haoranzhang001@gmail.com>
2024-12-20 02:08:11 +1100

    MoocApi 1.0.0.11 - Albert - add Menu model

--- Mooc.Model/Entity/Admin/Menu.cs
+++ Mooc.Model/Entity/Admin/Menu.cs
@@ -1,11 +1,21 @@
-
-
-
-using System;
+using Mooc.Shared.Enum;
 
 namespace Mooc.Model.Entity;
 
 public class Menu : BaseEntity
 {
+    public string? Title { get; set; }
+    public string? Permission { get; set; }
+    public MenuType MenuType { get; set; }
+    public string? Description { get; set; }
+    public string? Mark { get; set; }
+    public int OrderNum { get; set; } = 0;
+    public string? Route { get; set; }
+    public string? ComponentPath { get; set; }
+    public virtual ICollection<Menu> Children { get; set; } = new List<Menu>();
+    public long? ParentId { get; set; }
+    public Menu? Parent { get; set; }
+
+    //public virtual ICollection<RoleMenu> RoleMenus { get; set; } = new List<RoleMenu>();
 
 }

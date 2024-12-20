d2571542000505c73cacd18591798328164fbb98
Zhang-Haoran <haoranzhang001@gmail.com>
2024-12-20 12:16:51 +1100

    MoocApi 1.0.0.11 - Albert - formatting

--- Mooc.Model/MoocDbContextModelCreatingExtensions.cs
+++ Mooc.Model/MoocDbContextModelCreatingExtensions.cs
@@ -37,11 +37,7 @@ public static class MoocDbContextModelCreatingExtensions
                 v => (Access)Enum.Parse(typeof(Access), v));
             b.Property(x => x.IsActive).IsRequired().HasDefaultValue(true);
             b.Property(x => x.CreatedDate).IsRequired().HasDefaultValueSql("CURRENT_TIMESTAMP");//for SQLite
-
-
         });
-
-       
     }
 
     private static void ConfigureMenu(ModelBuilder modelBuilder)

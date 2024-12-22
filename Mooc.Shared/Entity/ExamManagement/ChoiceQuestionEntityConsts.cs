2335440318362036500996b8dc2011f229f5d4cc
DynamicPunch-AI <q754086006@gmail.com>
2024-12-22 21:17:42 +1100

    MoocModal 1.0.0.10: Davin - Created Migration for Choice question and Option Modal

---
+++ Mooc.Shared/Entity/ExamManagement/ChoiceQuestionEntityConsts.cs
@@ -0,0 +1,13 @@
+﻿namespace Mooc.Shared.Entity.ExamManagement
+{
+    public class ChoiceQuestionEntityConsts
+    {
+        public const int MaxQuestionTitleLength = 200;
+
+        public const int MaxQuestionBodyLength = 1600;
+
+        public const int MaxCorrectAnswerLength = 100;
+
+        public const int MaxMarkLength = 60;
+    }
+}

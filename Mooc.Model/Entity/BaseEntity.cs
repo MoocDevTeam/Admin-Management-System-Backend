c08a8a0d0614cacbd44689593ba715ae503438bc
DynamicPunch-AI <q754086006@gmail.com>
2024-12-23 22:22:47 +1100

    Revert "MoocModal 1.0.0.10: Davin - Created Migration for Choice question and Option Modal"
    This reverts commit 45c6d8101cf23958386e11948d93f1df21ca7382.

--- Mooc.Model/Entity/BaseEntity.cs
+++ Mooc.Model/Entity/BaseEntity.cs
@@ -4,10 +4,4 @@
 public class BaseEntity 
 {
     public long Id { get; set; }
-/*    public long? CreatedByUserId { get; set; }*/
-    public DateTime? CreatedAt { get; set; }
-/*    public long? UpdatedByUserId { get; set; }*/
-    public DateTime? UpdatedAt { get; set; }
-/*    public User CreatedByUser { get; set; }
-    public virtual ICollection<User> UpdatedByUsers { get; set; }*/
 }
--- Mooc.Shared/Entity/ExamManagement/OptionEntityConsts.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mooc.Shared.Entity.ExamManagement
-{
-    public class OptionEntityConsts
-    {
-        public const int MaxQuestionOrderLength = 60;
-
-        public const int MaxOptionValueLength = 900;
-    }
-}

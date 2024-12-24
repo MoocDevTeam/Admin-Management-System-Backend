a705ecbaf818b5b2fbe3d252fbbcc4e4ea06abaa
DynamicPunch-AI <q754086006@gmail.com>
2024-12-24 17:32:22 +1100

    MoocModal 1.0.0.18: Davin - Created Migration for Choice question and Option Modal - reverted

--- Mooc.Model/Entity/BaseEntity.cs
+++ Mooc.Model/Entity/BaseEntity.cs
@@ -1,13 +1,7 @@
 ﻿namespace Mooc.Model.Entity;
 
 
-public class BaseEntity 
+public class BaseEntity
 {
     public long Id { get; set; }
-    /*    public long? CreatedByUserId { get; set; }*/
-    public DateTime? CreatedAt { get; set; }
-    /*    public long? UpdatedByUserId { get; set; }*/
-    public DateTime? UpdatedAt { get; set; }
-    /*    public User CreatedByUser { get; set; }
-        public virtual ICollection<User> UpdatedByUsers { get; set; }*/
 }
--- Mooc.Model/Entity/ExamManagement/Option.cs
+++
@@ -1,9 +0,0 @@
-namespace Mooc.Model.Entity;
-
-public class Option : BaseEntity
-{
-    public long ChoiceQuestionId { get; set; }
-    public long OptionOrder { get; set; }
-    public string OptionValue { get; set; }
-    public virtual ICollection<ChoiceQuestion> ChoiceQuestions { get; set; }
-}

404235ac3e41857d7726e240978b6186f8fcd2a6
Yang Liu <69631635+changzzzzz@users.noreply.github.com>
2024-12-27 18:32:54 +1100

    MoocApi 1.0.0.24: Yang Course CRUD, Dto

--- Mooc.Application.Contracts/Course/Dto/Course/UpdateCourseDto.cs
+++ Mooc.Application.Contracts/Course/Dto/Course/UpdateCourseDto.cs
@@ -13,12 +13,6 @@ namespace Mooc.Application.Contracts.Course
         public string CourseCode { get; set; } = string.Empty;
         public string? CoverImage { get; set; }
         public string Description { get; set; } = string.Empty;
-        public User CreatedByUser { get; set; }
-        public User UpdatedByUser { get; set; }
-        public long CreatedByUserId { get; set; }
-        public long UpdatedByUserId { get; set; }
-        // public Category Category { get; set; }
-        public DateTime? CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
+
     }
 }

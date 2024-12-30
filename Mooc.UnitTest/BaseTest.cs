705d8d965850ec9b51eeac474f3fefdcfd459bb8
lilyjiangDev <jianglili2015@gmail.com>
2024-12-30 23:58:07 +1000

    MoocApi 1.0.0.22 Lily: Refactoring integration test

--- Mooc.UnitTest/BaseTest.cs
+++ Mooc.UnitTest/BaseTest.cs
@@ -6,9 +6,27 @@ namespace Mooc.UnitTest;
 public class BaseTest
 {
     protected MoocWebApplicationFactory Factory { get; private set; }
+    protected HttpClient Client { get; private set; }
     public BaseTest()
     {
         Factory = new MoocWebApplicationFactory();
+        Client = this.Factory.CreateClient();
+    }
+
+    [OneTimeTearDown]
+    protected void Clean()
+    {
+        if (Client != null)
+        {
+            Client.Dispose();
+            Client = null;
+        }
+
+        if (Factory != null)
+        {
+            Factory.Dispose();
+            Factory = null;
+        }
     }
 
     private JsonSerializerOptions serializeOptions = new JsonSerializerOptions

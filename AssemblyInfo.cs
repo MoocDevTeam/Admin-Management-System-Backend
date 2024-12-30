92842cf690265d022b93fec6fc1b1550fda4c748
lilyjiangDev <jianglili2015@gmail.com>
2024-12-30 23:23:49 +1000

    MoocApi 1.0.0.22 Lily: Fix integration test issue by changing the ef core version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Mooc Project Team")]
 [assembly: AssemblyProduct("Mooc")]
 [assembly: AssemblyCopyright("Copyright © Your Company 2024")]
-[assembly: AssemblyVersion("1.0.0.20")]
-[assembly: AssemblyFileVersion("1.0.0.20")]
+[assembly: AssemblyVersion("1.0.0.22")]
+[assembly: AssemblyFileVersion("1.0.0.22")]

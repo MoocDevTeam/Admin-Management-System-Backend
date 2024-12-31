a1da0fbfc36e49711f54ab3bdc3f7138f6b04614
DynamicPunch-AI <q754086006@gmail.com>
2024-12-31 13:22:10 +1100

    MoocModal 1.0.0.27: Davin - remove redundant assembly info

--- AssemblyInfo_BASE_74.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCompany("Mooc Project Team")]
-[assembly: AssemblyProduct("Mooc")]
-[assembly: AssemblyCopyright("Copyright © Your Company 2024")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]

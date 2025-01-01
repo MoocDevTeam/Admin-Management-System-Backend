bea593069277068a1930dcd34619d12578a68f67
lilyjiangDev <jianglili2015@gmail.com>
2025-01-01 10:50:10 +1000

    MoocApi 1.0.0.34: Lily - get the latest version number then add 1

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Mooc Project Team")]
 [assembly: AssemblyProduct("Mooc")]
 [assembly: AssemblyCopyright("Copyright © Your Company 2024")]
-[assembly: AssemblyVersion("1.0.0.33")]
-[assembly: AssemblyFileVersion("1.0.0.33")]
+[assembly: AssemblyVersion("1.0.0.34")]
+[assembly: AssemblyFileVersion("1.0.0.34")]

8eb3cd50acb6d53ed934aaab4d92d63a0e32062b
Yafei Deng <weihaianni@gmail.com>
2025-01-03 10:58:56 +1100

    MoocApi 1.0.0.34 Added endpoints for adding and getting all teachers for Teacher entity, added Dataseeding for teacher when table is empty

---
+++ Mooc.Application.Contracts/Course/ITeacherService.cs
@@ -0,0 +1,11 @@
+﻿using Mooc.Application.Contracts.Course.Dto;
+using Mooc.Model.Entity.Course;
+
+namespace Mooc.Application.Contracts.Course
+{
+    public interface ITeacherService : ICrudService<TeacherReadDto, TeacherReadDto, long, FilterPagedResultRequestDto, CreateOrUpdateTeacherDto, CreateOrUpdateTeacherDto>
+
+    {
+        Task<TeacherReadDto> GetTeacherByName(string teacher);
+    }
+}

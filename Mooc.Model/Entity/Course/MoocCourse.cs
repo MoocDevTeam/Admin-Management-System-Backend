4deff0d1a3550a98daa5b436df1957bc45a96035
Yang Liu <69631635+changzzzzz@users.noreply.github.com>
2025-01-03 16:19:14 +1100

    Moocapi 1.0.0.36: Yang fix CourseDBSeedDataService, courseMoodle

--- Mooc.Model/Entity/Course/MoocCourse.cs
+++ Mooc.Model/Entity/Course/MoocCourse.cs
@@ -9,15 +9,11 @@ public class MoocCourse : BaseEntity
     public string CourseCode { get; set; } = string.Empty;
     public string? CoverImage { get; set; }
     public string Description { get; set; } = string.Empty;
-    public User CreatedByUser { get; set; }
-    public User UpdatedByUser { get; set; }
+    public virtual User CreatedByUser { get; set; }
+    public virtual User UpdatedByUser { get; set; }
     public long CreatedByUserId { get; set; }
     public long UpdatedByUserId { get; set; }
-    // public Category Category { get; set; }
-    //public DateTime? CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
-
-    // Foreign key for Category
     public long CategoryId { get; set; }
     public virtual Category Category { get; set; } // Navigation property
 

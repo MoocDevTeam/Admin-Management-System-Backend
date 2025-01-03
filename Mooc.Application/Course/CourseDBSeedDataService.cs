df497d90991e4e8028f9d63b5f375cfb75ee1a4f
Yang Liu <69631635+changzzzzz@users.noreply.github.com>
2025-01-03 16:19:14 +1100

    Moocapi 1.0.0.36: Yang fix CourseDBSeedDataService, courseMoodle

--- Mooc.Application/Course/CourseDBSeedDataService.cs
+++ Mooc.Application/Course/CourseDBSeedDataService.cs
@@ -20,26 +20,27 @@ namespace Mooc.Application.Course
         // Add father table first. Example: MoocCourse has two foreign table: User, Category. Add User table -> Add Category table -> Add MoocCourse table
         private static List<User> users = new List<User>()
         {
-            new User(){Id=1, UserName="admin",Age=30,Email="admin@demo.com",Gender= Gender.Male, Password=BCryptUtil.HashPassword("123456")},
-            new User(){Id=2, UserName="test",Age=30,Email="test@demo.com",Gender= Gender.Male,Password=BCryptUtil.HashPassword("123456")},
+            new User(){Id=1, UserName="admin",Age=30,Email="admin@demo.com",Gender= Gender.Male, Password=BCryptUtil.HashPassword("123456"), Avatar= "some_url?"},
+            new User(){Id=2, UserName="test",Age=30,Email="test@demo.com",Gender= Gender.Male,Password=BCryptUtil.HashPassword("123456"), Avatar= "some_url?"},
         };
         private static List<Category> categories = new List<Category>()
         {
-            new Category(){Id=1, CategoryName="Category1",Description="",IconUrl="xxx@demo.com",CreatedByUser=users.First(u => u.Id == 1),CreatedAt= DateTime.Now },
-            new Category(){Id=2, CategoryName="Category2",Description="", ParentId=1,IconUrl="xxx@demo.com",CreatedByUser=users.First(u => u.Id == 1),CreatedAt= DateTime.Now },
+            new Category(){Id=1, CategoryName="Category1",Description="",IconUrl="xxx@demo.com",CreatedByUserId=1,CreatedAt= DateTime.Now },
+            new Category(){Id=2, CategoryName="Category2",Description="", ParentId=1,IconUrl="xxx@demo.com",CreatedByUserId=1,CreatedAt= DateTime.Now },
         };
 
         private static List<MoocCourse> courses = new List<MoocCourse>()
         {
-            new MoocCourse(){Id=1, Title="React",CourseCode="100",CoverImage="xxx.png",Description="React",CreatedByUser=users.First(u => u.Id == 1) ,UpdatedByUser=users.First(u => u.Id == 1),Category= categories.First(u => u.Id == 1),CreatedAt=DateTime.Now ,UpdatedAt=DateTime.Now.AddMinutes(1)},
-            new MoocCourse(){Id=2, Title=".Net",CourseCode="101",CoverImage="xxx.png",Description=".Net",CreatedByUser= users.First(u => u.Id == 1),UpdatedByUser=users.First(u => u.Id == 1), Category= categories.First(u => u.Id == 1),CreatedAt=DateTime.Now ,UpdatedAt=DateTime.Now.AddMinutes(1)},
-            new MoocCourse(){Id=3, Title="Nodejs",CourseCode="102",CoverImage="xxx.png",Description="Nodejs",CreatedByUser= users.First(u => u.Id == 1),UpdatedByUser=users.First(u => u.Id == 1), Category= categories.First(u => u.Id == 1),CreatedAt=DateTime.Now ,UpdatedAt=DateTime.Now.AddMinutes(1)},
+            new MoocCourse() { Id = 1, Title = "React", CourseCode = "100", CoverImage = "xxx.png", Description = "React", CreatedByUserId = 1, UpdatedByUserId = 1,CategoryId=1, CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now.AddMinutes(1) },
+            new MoocCourse() { Id = 2, Title = ".Net", CourseCode = "101", CoverImage = "xxx.png", Description = ".Net", CreatedByUserId = 1, UpdatedByUserId = 1, CategoryId=1, CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now.AddMinutes(1) },
+            new MoocCourse() { Id = 3, Title = "Nodejs", CourseCode = "102", CoverImage = "xxx.png", Description = "Nodejs", CreatedByUserId = 1, UpdatedByUserId = 1,CategoryId=1, CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now.AddMinutes(1) },
         };
+
 
         private List<CourseInstance> courseInstances = new List<CourseInstance>()
         {
-            new CourseInstance(){Id=1, MoocCourseId=1, TotalSessions=10, Status=CourseInstanceStatus.Open, Permission=CourseInstancePermission.Private, StartDate=DateTime.Now, EndDate=DateTime.Now.AddMonths(1), CreatedByUser=users.First(u => u.Id == 1) ,UpdatedByUser=users.First(u => u.Id == 1), CreatedAt=DateTime.Now, UpdatedAt=DateTime.Now  },
-            new CourseInstance(){Id=2, MoocCourseId=2, TotalSessions=20, Status=CourseInstanceStatus.Close, Permission=CourseInstancePermission.Public, StartDate=DateTime.Now, EndDate=DateTime.Now.AddMonths(2), CreatedByUser=users.First(u => u.Id == 1) ,UpdatedByUser=users.First(u => u.Id == 1), CreatedAt=DateTime.Now, UpdatedAt=DateTime.Now  },
+            new CourseInstance(){Id=1, MoocCourseId=1, TotalSessions=10, Status=CourseInstanceStatus.Open, Permission=CourseInstancePermission.Private, StartDate=DateTime.Now, EndDate=DateTime.Now.AddMonths(1), CreatedByUserId=1 ,UpdatedByUserId=1, CreatedAt=DateTime.Now, UpdatedAt=DateTime.Now  },
+            new CourseInstance(){Id=2, MoocCourseId=2, TotalSessions=20, Status=CourseInstanceStatus.Close, Permission=CourseInstancePermission.Public, StartDate=DateTime.Now, EndDate=DateTime.Now.AddMonths(2), CreatedByUserId=1 ,UpdatedByUserId=2, CreatedAt=DateTime.Now, UpdatedAt=DateTime.Now  },
         };
 
         private List<Enrollment> enrollments = new List<Enrollment>()
@@ -217,21 +218,33 @@ namespace Mooc.Application.Course
         public async Task<bool> InitAsync()
         {
 
-            if (_dbContext.MoocCourses.Any() || _dbContext.CourseInstances.Any() || _dbContext.Teachers.Any()) // check existing data
+            if (!_dbContext.MoocCourses.Any())
             {
-                return true; // if exist, skip seeding
-            }
-            else
-            {
-                //await this._dbContext.Users.AddRangeAsync(users);
-                //await this._dbContext.MoocCourses.AddRangeAsync(courses);
-                //await this._dbContext.CourseInstances.AddRangeAsync(courseInstances);
-                //await this._dbContext.Category.AddRangeAsync(categories);
-                //await this._dbContext.Enrollment.AddRangeAsync(enrollments);
-                await this._dbContext.Teachers.AddRangeAsync(teachers);
+                await this._dbContext.MoocCourses.AddRangeAsync(courses);
                 await this._dbContext.SaveChangesAsync();
             }
 
+            if (!_dbContext.Category.Any())
+            {
+                await this._dbContext.Category.AddRangeAsync(categories);
+                await this._dbContext.SaveChangesAsync();
+            }
+            if (!_dbContext.CourseInstances.Any())
+            {
+                await this._dbContext.CourseInstances.AddRangeAsync(courseInstances);
+                await this._dbContext.SaveChangesAsync();
+            }
+
+            if (!_dbContext.Teachers.Any())
+            {
+                await this._dbContext.Teachers.AddRangeAsync(teachers);
+                await this._dbContext.SaveChangesAsync();
+            }
+            if (!_dbContext.Enrollment.Any())
+            {
+                await this._dbContext.Enrollment.AddRangeAsync(enrollments);
+                await this._dbContext.SaveChangesAsync();
+            }
             return true;
         }
     }

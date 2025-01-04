bd08fd6069612204f6985d082635b5168653dc49
Yafei Deng <weihaianni@gmail.com>
2025-01-04 07:26:35 -0800

    MoocApi 1.0.0.40: Sprint2 completed. (#94)
    * MoocApi 1.0.0.35 : Implement all endpoints for teacher controller and added dataseeding method for teacher table when there is no data
    
    * Moocapi 1.0.0.36: Yang fix CourseDBSeedDataService, courseMoodle
    
    * Moocapi 1.0.0.36: Yang fix CourseDBSeedDataService, courseMoodle
    
    * Moocapi 1.0.0.36: yang fix reset
    
    * Moocapi 1.0.0.36 fix
    
    * fix:added updateByUser in enrollment model
    
    * MoocApi 1.0.0.38 kwon: CourseInstance CRUD is done (#88)
    
    * feat: add media model
    
    * MoocApi 1.0.0.22: renamed and modified CourseInstance related files (#56)
    
    * fix:removed ActionResult in Enrollment and Category Controllers
    
    * fix:added relation to courseInstance from enrollment
    
    * Moocapi 1.0.0.39: Milly-Created Seeddata, SessionDto, Interface, Service and Controller for session create funcltion
    
    * MoocApi 1.0.0.40: Added TeacherCourseInstance related dto and implemented Update and Get endpoints(Created endpoints need further discussion )
    
    ---------
    
    Co-authored-by: Yang Liu <69631635+changzzzzz@users.noreply.github.com>
    Co-authored-by: Laura <laurajiao7@gmail.com>
    Co-authored-by: KwonZhu <82878295+KwonZhu@users.noreply.github.com>
    Co-authored-by: Ray Runze Wang <rayrunzewang@gmail.com>
    Co-authored-by: Laura <162672533+laurajiao@users.noreply.github.com>
    Co-authored-by: Milly Chen <millychenau7@gmail.com>

--- Mooc.Application/Course/CourseProfile.cs
+++ Mooc.Application/Course/CourseProfile.cs
@@ -1,5 +1,4 @@
 ﻿using Mooc.Application.Contracts.Course.Dto.Category;
-using Mooc.Model.Entity.Course;
 using Mooc.Application.Contracts.Course.Dto;
 
 namespace Mooc.Application.Course;
@@ -13,13 +12,11 @@ public class CourseProfile : Profile
         CreateMap<CreateEnrollmentDto, Enrollment>();
         CreateMap<UpdateEnrollmentDto, Enrollment>();
 
-
         CreateMap<CourseDto, MoocCourse>();
         CreateMap<MoocCourse, CourseDto>();
         CreateMap<CreateCourseDto, MoocCourse>();
         CreateMap<MoocCourse, CreateCourseDto>();
         CreateMap<UpdateCourseDto, MoocCourse>();
-
 
         CreateMap<Category, CategoryDto>()
                  .ForMember(dest => dest.Courses, opt => opt.MapFrom(src => src.Courses));
@@ -38,10 +35,32 @@ public class CourseProfile : Profile
             .ForMember(dest => dest.CreatedByUser, opt => opt.MapFrom(src => src.CreatedByUser != null ? src.CreatedByUser.UserName : null))
             .ForMember(dest => dest.UpdatedByUser, opt => opt.MapFrom(src => src.UpdatedByUser != null ? src.UpdatedByUser.UserName : null));
 
+        //TeacherCourseInstance mapping
+        //Frontend---> Database
+        CreateMap<TeacherCourseInstanceCreateOrUpdateDto, TeacherCourseInstance>()
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(_ => DateTime.UtcNow))
+            .ForMember(dest => dest.CreatedByUserId, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(_ => DateTime.UtcNow))
+            .ForMember(dest => dest.UpdatedByUserId, opt => opt.Ignore());
+            
+        //Database ---> Frontend
+        CreateMap<TeacherCourseInstance, TeacherCourseInstanceReadDto>()
+            .ForMember(dest => dest.CreatedByUser, opt => opt.MapFrom(src => src.CreatedByUser != null ? src.CreatedByUser.UserName : null))
+            .ForMember(dest => dest.UpdatedByUser, opt => opt.MapFrom(src => src.UpdatedByUser != null ? src.UpdatedByUser.UserName : null));
 
-
-        //CreateMap<CreateUserDto, User>();
-        //CreateMap<UpdateUserDto, User>();
+        CreateMap<TeacherCourseInstance, TeacherCourseInstancePermissionDto>();
+        
+        // Session Mapping
+        // Frontend -> Backend -> Database 
+        CreateMap<CreateOrUpdateSessionDto, Session>()
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(_ => DateTime.UtcNow))
+            .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(_ => DateTime.UtcNow));
+        //  Database  -> Backend -> Frontend
+        CreateMap<Session, ReadSessionDto>();
+        
+        // CourseInstance Mapping
+        CreateMap<CourseInstance, CourseInstanceDto>();        
+        CreateMap<UpdateCourseInstanceDto, CourseInstance>();
     }
 }
 

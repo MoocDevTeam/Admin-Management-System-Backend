c757e26bdfdb1df24956839014e2f5f3667292d9
Yang Liu <69631635+changzzzzz@users.noreply.github.com>
2025-01-11 20:38:29 +1100

    MoocApi 1.0.0.47 Yang fixed MoocCourse entity and dbcontext mapping

--- Mooc.Model/Entity/Course/MoocCourse.cs
+++ Mooc.Model/Entity/Course/MoocCourse.cs
@@ -8,13 +8,14 @@ public class MoocCourse : BaseEntityWithAudit
     public string CourseCode { get; set; } = string.Empty;
     public string? CoverImage { get; set; }
     public string Description { get; set; } = string.Empty;
+
+    // Nav User class later
     public virtual User CreatedByUser { get; set; }
     public virtual User UpdatedByUser { get; set; }
-    //public long CreatedByUserId { get; set; }
-    //public long UpdatedByUserId { get; set; }
-    //public DateTime? UpdatedAt { get; set; }
     public long CategoryId { get; set; }
     public virtual Category Category { get; set; } // Navigation property
+
+    // Nav to a collection of CourseInstance
     public virtual ICollection<CourseInstance> CourseInstances { get; set; }
 
 }

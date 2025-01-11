a766c50ba3859124f3843f82d8ef17cace20931a
Yang Liu <69631635+changzzzzz@users.noreply.github.com>
2025-01-11 20:38:29 +1100

    MoocApi 1.0.0.47 Yang fixed MoocCourse entity and dbcontext mapping

--- Mooc.Model/MoocDbContextModelCreatingExtensions.cs
+++ Mooc.Model/MoocDbContextModelCreatingExtensions.cs
@@ -308,22 +308,13 @@ public static class MoocDbContextModelCreatingExtensions
             b.Property(cs => cs.CourseCode).IsRequired().HasMaxLength(CourseEntityConsts.MaxCourseCodeLength);
             b.Property(cs => cs.CoverImage).IsRequired();
             b.Property(cs => cs.Description).HasMaxLength(CourseEntityConsts.MaxDescriptionLength);
-            b.Property(cs => cs.CreatedAt)
-            .ValueGeneratedOnAdd(); //  
-
-            b.Property(cs => cs.UpdatedAt)
-                .ValueGeneratedOnAddOrUpdate();
+            b.ConfigureAudit();
 
             //Foreign configuration temperarily use <User> until <MoocUser is created>
-            b.HasOne(x => x.CreatedByUser)
-            .WithMany()  // User has many CreatedCourses
-            .HasForeignKey(x => x.CreatedByUserId)  // Foreign key property
-            .OnDelete(DeleteBehavior.Restrict);  // Delete behavior
-
-            b.HasOne(x => x.UpdatedByUser)
-            .WithMany()  // User has many UpdatedCourses
-            .HasForeignKey(x => x.UpdatedByUserId)  // Foreign key property
-            .OnDelete(DeleteBehavior.Restrict);  // Delete behavior
+            b.HasOne<User>(x => x.CreatedByUser)
+            .WithMany()
+            .HasForeignKey(x => x.CreatedByUserId)
+            .OnDelete(DeleteBehavior.Cascade);
 
             b.HasOne(x => x.Category)  // One Category
             .WithMany(c => c.Courses) // Many Courses
@@ -385,7 +376,7 @@ public static class MoocDbContextModelCreatingExtensions
         modelBuilder.Entity<Enrollment>(b =>
         {
             b.ToTable(TablePrefix + "Enrollment");
-            b.HasKey(x => x.Id);          
+            b.HasKey(x => x.Id);
             b.Property(e => e.Id).ValueGeneratedNever();
             b.Property(e => e.CourseInstanceId).IsRequired();
             b.Property(e => e.CourseInstanceId).IsRequired();
@@ -398,7 +389,7 @@ public static class MoocDbContextModelCreatingExtensions
             b.Property(e => e.MaxStudents)
                 .IsRequired()
                 .HasMaxLength(300);
-            
+
             b.Property(e => e.CreatedAt)
                 .IsRequired()
                 .HasDefaultValueSql("GETDATE()");
@@ -429,7 +420,7 @@ public static class MoocDbContextModelCreatingExtensions
 
             // Properties
             b.Property(x => x.Title)
-                .IsRequired() 
+                .IsRequired()
                 .HasMaxLength(SessionEntityConsts.MaxTitleLength); // 50
 
             b.Property(x => x.Description)
@@ -443,13 +434,13 @@ public static class MoocDbContextModelCreatingExtensions
               .IsRequired();
 
             b.Property(x => x.CreatedByUserId)
-                .IsRequired();  
+                .IsRequired();
 
             b.Property(x => x.UpdatedByUserId)
                 .IsRequired(false);
 
             b.Property(x => x.CreatedAt)
-                .IsRequired() 
+                .IsRequired()
                 .HasDefaultValueSql("CURRENT_TIMESTAMP");
 
             b.Property(x => x.UpdatedAt)
@@ -467,7 +458,7 @@ public static class MoocDbContextModelCreatingExtensions
                 .HasForeignKey(x => x.UpdatedByUserId)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            b.HasOne<CourseInstance>( x => x.CourseInstance)
+            b.HasOne<CourseInstance>(x => x.CourseInstance)
                 .WithMany(s => s.Sessions)
                 .HasForeignKey(s => s.CourseInstanceId)
                 .OnDelete(DeleteBehavior.Cascade);
@@ -524,7 +515,7 @@ public static class MoocDbContextModelCreatingExtensions
                 .WithMany(x => x.TeacherCourseInstances)
                 .HasForeignKey(x => x.CourseInstanceId)
                 .OnDelete(DeleteBehavior.Cascade);
-            });
+        });
     }
 
     private static void ConfigureMedia(ModelBuilder modelBuilder)
@@ -574,14 +565,14 @@ public static class MoocDbContextModelCreatingExtensions
                 .WithMany()
                 .HasForeignKey(x => x.CreatedByUserId)
                 .OnDelete(DeleteBehavior.Cascade);
-        
+
             b.HasOne<User>(x => x.UpdatedByUser)
                 .WithMany()
                 .HasForeignKey(x => x.UpdatedByUserId)
                 .OnDelete(DeleteBehavior.Cascade);
 
             b.HasOne<Session>(x => x.Session)
-                .WithMany(x=>x.Sessionmedia)
+                .WithMany(x => x.Sessionmedia)
                 .HasForeignKey(x => x.SessionId)
                 .OnDelete(DeleteBehavior.Cascade);
         });
@@ -876,7 +867,7 @@ public static class MoocDbContextModelCreatingExtensions
             b.HasOne(eq => eq.Exam)
                 .WithMany(e => e.ExamQuestion)
                 .HasForeignKey(eq => eq.ExamId);
-           // we can choose either have 3 columns (ChoiceQuestionId, JudgementQuestionId, ShortAnsQuestionId) or have 1 column (questionId)
+            // we can choose either have 3 columns (ChoiceQuestionId, JudgementQuestionId, ShortAnsQuestionId) or have 1 column (questionId)
             b.HasOne(m => m.ChoiceQuestion)
                 .WithMany()
                 .HasForeignKey(eq => eq.ChoiceQuestionId);
@@ -887,8 +878,8 @@ public static class MoocDbContextModelCreatingExtensions
                 .WithMany()
                 .HasForeignKey(eq => eq.ShortAnsQuestionId);
             // 3 columns (ChoiceQuestionId, JudgementQuestionId, ShortAnsQuestionId) like above commented
-/*            b.Property(x => x.QuestionType)
-                .IsRequired();*/
+            /*            b.Property(x => x.QuestionType)
+                            .IsRequired();*/
             // 1 column  (questionId) when use add controller, frontend need to send / backend controller need to accept 1 extra parameter QuestionType
             b.HasOne(eq => eq.CreatedByUser)
                 .WithMany()

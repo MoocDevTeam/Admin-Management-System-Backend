299d3c302b48474f05c3baad38319a0901b176a1
Ray Runze Wang <rayrunzewang@gmail.com>
2025-01-14 22:07:37 +1100

    MoocApi 1.0.0.54 Ray: add course data

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Mooc Project Team")]
 [assembly: AssemblyProduct("Mooc")]
 [assembly: AssemblyCopyright("Copyright © Your Company 2024")]
-[assembly: AssemblyVersion("1.0.0.49")]
-[assembly: AssemblyFileVersion("1.0.0.49")]
+[assembly: AssemblyVersion("1.0.0.54")]
+[assembly: AssemblyFileVersion("1.0.0.54")]
 
 
 

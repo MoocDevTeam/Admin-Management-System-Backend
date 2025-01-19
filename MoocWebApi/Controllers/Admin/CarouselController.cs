4312c5533cf60e706c1b541041c0afab2d1cd9b8
Andy Wang <95andyw@gmail.com>
2025-01-19 17:21:19 +1000

    edit GroupName as AdminService for Admin Controllers

--- MoocWebApi/Controllers/Admin/CarouselController.cs
+++ MoocWebApi/Controllers/Admin/CarouselController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MoocWebApi.Controllers.Admin
 {
+    [ApiExplorerSettings(GroupName = nameof(SwaggerGroup.AdminService))]
     [Route("api/[controller]/[action]")]
     [ApiController]
     public class CarouselController : ControllerBase

3dc8483d7a166203bd13e4a2b3177351b40f6778
Andy Wang <95andyw@gmail.com>
2025-01-20 12:17:06 +1000

    feat: add CarouselDto with properties and validation

--- Mooc.Application.Contracts/Admin/Dto/Carousel/CarouselDto.cs
+++ Mooc.Application.Contracts/Admin/Dto/Carousel/CarouselDto.cs
@@ -1,7 +1,13 @@
-
 namespace Mooc.Application.Contracts.Admin;
 
 public class CarouselDto : BaseEntityDto
 {
+    public string Title { get; set; } = "Untitled";
+    public string? ImageUrl { get; set; }
+    public string? RedirectUrl { get; set; }
+    public bool IsActive { get; set; } = false;
+    public int Position { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
 
 }

63f9fe0437e7084fe030e3ddd5a87ee8e18fa8ce
Yang Liu <69631635+changzzzzz@users.noreply.github.com>
2025-01-22 08:54:32 +1100

    MoocApi 1.0.0.61 Yang：Modify Course model and its Dtos

--- Mooc.Application.Contracts/Course/Dto/Course/UpdateCourseDto.cs
+++ Mooc.Application.Contracts/Course/Dto/Course/UpdateCourseDto.cs
@@ -2,7 +2,11 @@ namespace Mooc.Application.Contracts.Course;
 
 public class UpdateCourseDto : BaseEntityDto
 {
+    [Required(ErrorMessage = "Title is null")]
+    [StringLength(100, ErrorMessage = "Title must be less than 100 characters")]
     public string Title { get; set; } = string.Empty;
+    [Required(ErrorMessage = "CourseCode is null")]
+    [StringLength(100, ErrorMessage = "CourseCode must be less than 100 characters")]
     public string CourseCode { get; set; } = string.Empty;
     public string? CoverImage { get; set; }
     public string Description { get; set; } = string.Empty;

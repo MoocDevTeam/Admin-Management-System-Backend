bedf1675a14af07ccde9704b5b7998f5b5670050
Raymond <zhuyuleiraymond@outlook.com>
2025-01-27 14:13:34 +1100

    feat: Add Multiple Choice Question type and CRUD services

---
+++ Mooc.Application.Contracts/ExamManagement/Dto/MultipleChoiceQuestion/CreateMultipleChoiceQuestionDto.cs
@@ -0,0 +1,34 @@
+using System.Text.Json.Serialization;
+
+namespace Mooc.Application.Contracts.ExamManagement;
+
+public class CreateMultipleChoiceQuestionDto : BaseEntityDto
+{
+    [JsonIgnore]
+    public override long Id { get; set; }
+    
+    public long? CourseId { get; set; }
+    
+    public long? CreatedByUserId { get; set; }
+    
+    [JsonIgnore]
+    public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+    
+    [JsonIgnore]
+    public long? UpdatedByUserId { get; set; }
+    
+    [JsonIgnore]
+    public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
+    
+    public string? QuestionBody { get; set; }
+    
+    public string? QuestionTitle { get; set; }
+    
+    public int Marks { get; set; }
+    
+    public long QuestionTypeId { get; set; }
+    
+    public string? CorrectAnswers { get; set; }
+    
+    public List<CreateOptionDto>? Options { get; set; }
+}

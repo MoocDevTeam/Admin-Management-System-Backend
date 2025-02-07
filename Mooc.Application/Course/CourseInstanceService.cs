19dc35e0e46b6b8119a977eaec80b50de9c9fcd9
KwonZhu <82878295+KwonZhu@users.noreply.github.com>
2025-02-07 19:05:51 +1030

    MoocApi 1.0.0.70 kwon: 1>modify CourseInstance related Dtos 2>Remove duplicate mothods and override MapToEntity in CourseInstanceService 3>add [Authorize] to CourseInstanceController (#192)

--- Mooc.Application/Course/CourseInstanceService.cs
+++ Mooc.Application/Course/CourseInstanceService.cs
@@ -3,7 +3,21 @@
     public class CourseInstanceService : CrudService<CourseInstance, CourseInstanceDto, CourseInstanceDto, long, FilterPagedResultRequestDto, CreateCourseInstanceDto, UpdateCourseInstanceDto>,
         ICourseInstanceService, ITransientDependency
     {
-        public CourseInstanceService(MoocDBContext dbContext, IMapper mapper) : base(dbContext, mapper) {}
+        public CourseInstanceService(MoocDBContext dbContext, IMapper mapper) : base(dbContext, mapper) { }
+
+        /// <summary>
+        /// Overrides the default MapToEntity method to invoke SetUpdatedAudit() while creating a new CourseInstance
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        protected override CourseInstance MapToEntity(CreateCourseInstanceDto input)
+        {
+            var entity = this.Mapper.Map<CreateCourseInstanceDto, CourseInstance>(input);
+            SetIdForLong(entity);
+            SetCreatedAudit(entity);
+            SetUpdatedAudit(entity);
+            return entity;
+        }
 
         /// <summary>
         /// Overrides the default CreateFilteredQuery method to include related entities while fetching CourseInstance
@@ -45,26 +59,26 @@
             return MapToGetOutputDto(courseInstance);
         }
 
-        public async Task<PagedResultDto<CourseInstanceDto>> GetListAsync(FilterPagedResultRequestDto input)
-        {
-            return await base.GetListAsync(input);
-        }
+        //public async Task<PagedResultDto<CourseInstanceDto>> GetListAsync(FilterPagedResultRequestDto input)
+        //{
+        //    return await base.GetListAsync(input);
+        //}
 
 
-        public async Task<CourseInstanceDto> CreateAsync(CreateCourseInstanceDto input)
-        {
-            return await base.CreateAsync(input);
-        }
+        //public async Task<CourseInstanceDto> CreateAsync(CreateCourseInstanceDto input)
+        //{
+        //    return await base.CreateAsync(input);
+        //}
 
-        public async Task<CourseInstanceDto> UpdateAsync(long id, UpdateCourseInstanceDto input)
-        {
-            return await base.UpdateAsync(id, input);
-        }
+        //public async Task<CourseInstanceDto> UpdateAsync(long id, UpdateCourseInstanceDto input)
+        //{
+        //    return await base.UpdateAsync(id, input);
+        //}
 
-        public async Task DeleteAsync(long id)
-        {
-            await base.DeleteAsync(id);
-        }
+        //public async Task DeleteAsync(long id)
+        //{
+        //    await base.DeleteAsync(id);
+        //}
 
         //public async Task<List<CourseInstanceDto>> GetByMoocCourseTtileAsync(string moocCourseTtile)
         //{

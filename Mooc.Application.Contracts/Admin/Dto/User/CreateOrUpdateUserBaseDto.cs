ecce15acd9d99eaef4e8ea9b54572cb3d3214b95
lilyjiangDev <jianglili2015@gmail.com>
2025-02-10 23:55:58 +1000

    update user issue resolved

--- Mooc.Application.Contracts/Admin/Dto/User/CreateOrUpdateUserBaseDto.cs
+++ Mooc.Application.Contracts/Admin/Dto/User/CreateOrUpdateUserBaseDto.cs
@@ -9,14 +9,14 @@ public class CreateOrUpdateUserBaseDto : BaseEntityDto
 
     // [Required(ErrorMessage = "Password is null")]
     // [StringLength(100, ErrorMessage = "Password  must be less than 100 characters")]
-    public virtual string Password { get; set; }
+  
     public int Age { get; set; }
 
     public string? Email { get; set; }
     public Access Access { get; set; }
     public Gender Gender { get; set; }
 
-    public string? Avatar { get; set; }
+
 
     public DateTime? CreatedDate { get; set; }
     public bool IsActive { get; set; }

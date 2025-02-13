1f12bd7bdbfdbb3610965bf6c84f29faeb368253
Zhe Wang <140377368+AndyZheWang@users.noreply.github.com>
2025-02-13 12:43:02 +1000

    Team01 (#195)
    * MoocApi 1.0.0.18: Andy - Edit gitignore
    
    * modify create and update method in userservice to assign roles
    
    * change BaseEntity to BaseEntityWithAudit in user.cs
    
    * add cascade deletion for userrole and create deleteasync for userservice
    
    * MoocApi 1.0.0.38 Michael: update .ignore file
    
    * Fixed carousel entity and comment entity.
    
    * MoocApi 1.0.0.49 Michael: modified role controller update method from put to post, did the same to the update test. No need to inherit BaseEntityWithAudit.
    
    * Fixed bug of carousel entity and comment entity.
    
    * Separate the seed for comments into an independent file.
    
    * rewrote createAsync test to test role assignment
    
    * MoocApi 1.0.0.50: Andy - Modify the Carousel and Comment models, separate the Comment seed service, adjust the call order to 9, and associate it with BaseEntityWithAudit. (#133)
    
    * Fixed carousel entity and comment entity.
    
    * Fixed bug of carousel entity and comment entity.
    
    * Separate the seed for comments into an independent file.
    
    * added unit test for updateAsync for valid roleId
    
    * added unit test for createAsync with invalid role ids
    
    * added unit test for updateAsync with invalid role ids
    
    * added unit tests for deleteAsync
    
    * edit version to 1.0.0.50
    
    * resolve conflict
    
    * WebAPI - Albert - 1.0.0.50 : apply entityWithAudit
    
    * apply baseentitywithaudit to user.cs
    
    * modify userendpointtest to avoid duplicated userId
    
    * refactor userendpoint test to include unique userId generation
    
    * refactor userendpoint test to include unique userId generation
    
    * MoocApi 1.0.0.52 Xiaodan: Login refactoring
    
    * Mooc.WebApi 1.0.0.57: Della - Changed the not found exception from userController to userService.
    
    * modified moocwebapi.csproj
    
    * edit GroupName as AdminService for Admin Controllers
    
    * MOOCApi 1.0.0.58 Michael- add Api response in Auth controller
    
    * MOOCAPI 1.0.0.57 Michael-Bugfix: fix userService test calling baseAync(input)
    
    * MOOCAPI 1.0.0.58 Michael-Bugfix: add attributes validation for createOrUpdateRoleDto, deleted SetCreatedAudit in MenuService
    
    * MOOCAPI 1.0.0.67 Lily: Refactoring code -06/02/2025
    
    * change localhost port to 3000 in program.cs
    
    * adjust user tests for modified dto
    
    * MOOCWebApi 1.0.0.66 Michael-deleted menu controller try catch code and added ioptions for JWT config
    
    * MOOCWebapi 1.0.0.67 Michael Refactor:1) add bulk delete to role controller and role service; 2) delete Menu try catch; 3) add Ioptions config for JWT
    
    * commented out password requirement in createorupdateuserdto
    
    * update user issue resolved
    
    * MOOCWebApi 1.0.0.67 MIchael: Refactor- changed cors origin url
    
    * MoocApi 1.0.0.68: Della- Add avatar service and controller
    
    - Add `IAvatarService`, `AvatarService`, `AvatarController`
    - Change version to 1.0.0.68
    - Unit test to be done.
    
    * MoocApi 1.0.0.68: Della: Changed as per Comment.
    
    ---------
    
    Co-authored-by: tailesiweift <xuanrong.sharon@gmail.com>
    Co-authored-by: michaelchen943 <michael.chen394@aliyun.com>
    Co-authored-by: DC <michael_chen943@aliyun.com>
    Co-authored-by: Haoran Zhang <haoranzhang001@gmail.com>
    Co-authored-by: Sharon Z <159991786+tailesiweift@users.noreply.github.com>
    Co-authored-by: Xiaodan Cao <caoxiaodan@hotmail.com>
    Co-authored-by: DESKTOP-LLHTIO6\chels <dengsha363618@gmail.com>
    Co-authored-by: Della363618 <132197908+Della363618@users.noreply.github.com>
    Co-authored-by: lilyjiangDev <jianglili2015@gmail.com>

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,9 +1,10 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Mooc Project Team")]
 [assembly: AssemblyProduct("Mooc")]
 [assembly: AssemblyCopyright("Copyright © Your Company 2024")]
+
 
 [assembly: AssemblyVersion("1.0.0.70")]
 [assembly: AssemblyFileVersion("1.0.0.70")]

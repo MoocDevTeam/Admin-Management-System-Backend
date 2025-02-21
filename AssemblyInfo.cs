60de996a10ab9d43c12c97eebf3532364d6b797c
DESKTOP-LLHTIO6\chels <dengsha363618@gmail.com>
2025-02-21 11:33:04 +1030

    MoocApi 1.0.0.74: Della: security conflict resolved (remove s3 bucket keys) and version into1.0.0.74

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Mooc")]
 [assembly: AssemblyCopyright("Copyright © Your Company 2024")]
 
-[assembly: AssemblyVersion("1.0.0.72")]
-[assembly: AssemblyFileVersion("1.0.0.72")]
+[assembly: AssemblyVersion("1.0.0.74")]
+[assembly: AssemblyFileVersion("1.0.0.74")]
 
 
 

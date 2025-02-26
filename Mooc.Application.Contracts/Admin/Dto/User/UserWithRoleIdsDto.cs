f39061cd87ded77c28484fb4dbdee4e37d30fedd
lilyjiangDev <jianglili2015@gmail.com>
2025-02-26 23:52:49 +1000

    MoocApi 1.0.0.78 1> Bug Fix for the common cache removal method 2> Add an endpoint to retrieve complete user information, including roles, by user ID

---
+++ Mooc.Application.Contracts/Admin/Dto/User/UserWithRoleIdsDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mooc.Application.Contracts.Admin.Dto.User;
+
+public class UserWithRoleIdsDto: UserDto
+{
+    public List<long> RoleIds { get; set; } = new List<long>();
+}

9dffeafa07aa04cce9b8656efff8299a6967be7e
sharon <xuanrong.sharon@gmail.com>
2025-03-02 17:09:21 +0800

    MoocWEBAPI 1.0.0.81 -Sharon : refactor AddUser endpoint to enable associated role IDs persisting into userrole

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Mooc")]
 [assembly: AssemblyCopyright("Copyright © Your Company 2024")]
 
-[assembly: AssemblyVersion("1.0.0.80")]
-[assembly: AssemblyFileVersion("1.0.0.80")]
+[assembly: AssemblyVersion("1.0.0.81")]
+[assembly: AssemblyFileVersion("1.0.0.81")]
 
 
 
